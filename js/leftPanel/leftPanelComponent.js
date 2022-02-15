1845f10c2fdcf78330e1a344b6b6f39ba6b34085
sergisa <phebra@yandex.ru>
2022-02-15 11:56:40 +0300

    Левая панель перенесена в отдельный компонент

---
+++ js/leftPanel/leftPanelComponent.js
@@ -0,0 +1,6 @@
+calendarApp.component('leftPanel', {
+    templateUrl: 'js/leftPanel/leftPanel.template.html',
+    controller: function LeftPanelController($scope, $element, $attrs) {
+
+    }
+});
